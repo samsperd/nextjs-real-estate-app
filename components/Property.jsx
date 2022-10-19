d59bbb0ec99c65129b31cd22b1941829e350d417
samsperd <iloejeobinna@gmail.com>
2022-10-19 16:11:06 +0100

    last commit

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,6 +1,5 @@
-import Link from "next/link"
 import Image from "next/image"
-import { Box, Flex, Text, Avatar } from "@chakra-ui/react"
+import { Box, Flex, Text, Avatar, Link } from "@chakra-ui/react"
 import { FaBed, FaBath } from "react-icons/fa"
 import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
